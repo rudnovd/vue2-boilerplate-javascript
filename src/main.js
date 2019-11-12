67af6406396220f1279458714688dcc74b13627c
rudnovd <rudnov.d@gmail.com>
2019-11-13 01:42:33 +0500

    feat(app): add sentry

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
+import "./scss/main.scss";
 import Vue from "vue";
 import App from "./App";
 import router from "./router";
 import store from "./store";
-
-import "./scss/main.scss";
+import * as Sentry from "@sentry/browser";
+import * as Integrations from "@sentry/integrations";
 
 import "./globalComponents";
 
@@ -14,3 +15,10 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("body");
+
+if (process.env.VUE_APP_SENTRY_DSN && process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+  });
+}
