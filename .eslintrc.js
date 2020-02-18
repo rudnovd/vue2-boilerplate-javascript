8cb8a8e1f1ccaf57560f71258aea6342bea5b2bd
rudnovd <rudnov.d@gmail.com>
2020-02-18 16:41:39 +0500

    build(dependencies): remove Babel dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,19 @@ module.exports = {
   env: {
     node: true
   },
+  parserOptions: {
+    ecmaVersion: 2020
+  },
   extends: ['plugin:vue/essential', 'plugin:vue/strongly-recommended', 'plugin:vue/recommended', '@vue/prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
-  parserOptions: {
-    parser: "babel-eslint"
-  },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
