22b2baca1f880bf451de966e04680e4febff46db
rudnovd <rudnov.d@gmail.com>
2020-02-28 00:24:55 +0500

    feat(components): add BaseSpinner component

---
+++ src/components/base/BaseSpinner.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="base-spinner" :style="style" />
+</template>
+
+<script>
+export default {
+  name: 'BaseSpinner',
+  props: {
+    size: {
+      type: String,
+      required: false,
+      default: '1rem'
+    }
+  },
+  computed: {
+    style() {
+      return {
+        height: this.size,
+        width: this.size
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.base-spinner {
+  display: inline-block;
+  border: 3px solid;
+  border-color: rgb(0, 0, 0);
+  border-top-color: rgba(0, 0, 0, 0.3);
+  animation: spin 2s linear infinite;
+  border-radius: 50%;
+  opacity: 0.2;
+
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+}
+</style>
