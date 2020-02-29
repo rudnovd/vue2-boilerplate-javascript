ba5fffd8e39cc4df2bdc76a95cb4c8a775368930
rudnovd <rudnov.d@gmail.com>
2020-02-29 20:10:37 +0500

    feat(views): add styles and links for Error page

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,13 +1,30 @@
 <template>
-  <section class="error">
-    <p>
-      Error
-    </p>
-  </section>
+  <body>
+    <section class="error-page">
+      <span>Page not found</span>
+      <router-link to="/">Home page</router-link>
+    </section>
+  </body>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Error'
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.error-page {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+
+  span {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
+}
+</style>
