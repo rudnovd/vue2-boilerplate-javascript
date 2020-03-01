b8445a25815ca389d521c80cf03046fc24e773ed
rudnovd <rudnov.d@gmail.com>
2020-03-01 21:03:26 +0500

    fix(globalComponents): remove loading-spinner from global components

--- src/globalComponents.js
+++ src/globalComponents.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 
 import BaseLayout from '@/layouts/BaseLayout'
 import UserLayout from '@/layouts/UserLayout'
-import LoadingSpinner from '@/components/base/LoadingSpinner'
 
-Vue.component('loading-spinner', LoadingSpinner)
 Vue.component('BaseLayout', BaseLayout)
 Vue.component('UserLayout', UserLayout)
